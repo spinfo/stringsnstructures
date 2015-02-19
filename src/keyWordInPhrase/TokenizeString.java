16cd4f5baf79764459ad9796fce5ce8be1e1fceb
Mandy Neumann <mandy@aphrodite.spinfo.uni-koeln.de>
2015-02-19 16:03:08 +0100

    Replace Sysout with Log Messages in KWIP component

--- src/keyWordInPhrase/TokenizeString.java
+++ src/keyWordInPhrase/TokenizeString.java
@@ -13,23 +13,22 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 
 public class TokenizeString {
-	
-	public TokenizeString() {}
 
-	  public static List<String> tokenizeString(Analyzer analyzer, String string) {
-	    List<String> result = new ArrayList<String>();
-	    try {
-	      TokenStream stream  = analyzer.tokenStream(null, new StringReader(string));
-	      stream.reset();
-	      while (stream.incrementToken()) {
-	        result.add(stream.getAttribute(CharTermAttribute.class).toString());
-	      }
-	      stream.close();
-	    } catch (IOException e) {
-	      // not thrown b/c we're using a string reader...
-	      throw new RuntimeException(e);
-	    }
-	    return result;
-	  }
-	   
+	public static List<String> tokenizeString(Analyzer analyzer, String string) {
+		List<String> result = new ArrayList<String>();
+		try {
+			TokenStream stream = analyzer.tokenStream(null, new StringReader(
+					string));
+			stream.reset();
+			while (stream.incrementToken()) {
+				result.add(stream.getAttribute(CharTermAttribute.class)
+						.toString());
+			}
+			stream.close();
+		} catch (IOException e) {
+			// not thrown b/c we're using a string reader...
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
 }
