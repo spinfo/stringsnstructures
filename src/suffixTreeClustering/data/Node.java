693702ac02d89410ae973d7c02206928f6243e92
Mandy Neumann <mandy@aphrodite.spinfo.uni-koeln.de>
2015-03-19 13:51:24 +0100

    Add javadoc

--- src/suffixTreeClustering/data/Node.java
+++ src/suffixTreeClustering/data/Node.java
@@ -44,6 +44,11 @@ public class Node implements Comparable<Node> {
 				+ " :: document frequency: " + df;
 	}
 
+	/*
+	 * Nodes are compared by their ID. (non-Javadoc)
+	 * 
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	@Override
 	public int compareTo(Node o) {
 		return this.nodeNumber - o.nodeNumber;
@@ -53,7 +58,7 @@ public class Node implements Comparable<Node> {
 	 * Get Type by ID.
 	 * 
 	 * @param number
-	 *            - Tyoe ID
+	 *            - Type ID
 	 * @return Type
 	 */
 	public Type getType(Integer number) {
@@ -64,14 +69,35 @@ public class Node implements Comparable<Node> {
 		return null;
 	}
 
+	/**
+	 * Get all Types that visited this node together with the start positions in
+	 * text.
+	 * 
+	 * @return mapping of types and start positions
+	 */
 	public Map<Type, List<Integer>> getTypes() {
 		return typesAndFrequencies;
 	}
 
+	/**
+	 * Sets the node number as unique ID
+	 * 
+	 * @param number
+	 *            node number
+	 */
 	public void setNodeNumber(int number) {
+		if (this.nodeNumber != null)
+			throw new UnsupportedOperationException(
+					"Node number is unique and should not be set twice!");
 		this.nodeNumber = number;
 	}
 
+	/**
+	 * Sets the node's path label.
+	 * 
+	 * @param label
+	 *            path label of node
+	 */
 	public void setPathLabel(String label) {
 		this.pathLabel = label;
 	}
@@ -88,10 +114,20 @@ public class Node implements Comparable<Node> {
 		this.df = df;
 	}
 
+	/**
+	 * Get the node's unique number.
+	 * 
+	 * @return node number
+	 */
 	public int getNodeNumber() {
 		return this.nodeNumber;
 	}
 
+	/**
+	 * Get number of different types that share this node = document frequency
+	 * 
+	 * @return document frequency df
+	 */
 	public Integer getDF() {
 		return this.df;
 	}
@@ -106,20 +142,43 @@ public class Node implements Comparable<Node> {
 		return this.typesAndFrequencies.keySet().contains(type);
 	}
 
+	/**
+	 * Get all start positions of the suffix of a particular type.
+	 * 
+	 * @param type
+	 * @return List of start positions
+	 */
 	public List<Integer> getStartPositionsOfType(Type type) {
 		return this.typesAndFrequencies.get(type);
 	}
 
-	public Integer getTermfrequencyFor(Type document) {
-		if (this.typesAndFrequencies.get(document) != null)
-			return this.typesAndFrequencies.get(document).size();
+	/**
+	 * Get the number of times a specific type visited this node.
+	 * 
+	 * @param type
+	 * @return number of visits
+	 */
+	public Integer getTermfrequencyFor(Type type) {
+		if (this.typesAndFrequencies.get(type) != null)
+			return this.typesAndFrequencies.get(type).size();
 		return null;
 	}
 
+	/**
+	 * Set the number of total visits for this node.
+	 * 
+	 * @param freq
+	 *            frequency = number of visits
+	 */
 	public void setFrequency(int freq) {
 		this.frequency = freq;
 	}
 
+	/**
+	 * Get the number of total visits for this node.
+	 * 
+	 * @return frequency = number of visits
+	 */
 	public int getFrequency() {
 		return frequency;
 	}
