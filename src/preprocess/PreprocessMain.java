31bd69cbc7fd109acc0e6fdbb57069a755c72f47
Marcel Boeing <mboeing3@uni-koeln.de>
2015-04-30 09:23:41 +0200

    added main

--- src/preprocess/PreprocessMain.java
+++ src/preprocess/PreprocessMain.java
@@ -14,6 +14,10 @@ import util.TextInfo;
 public class PreprocessMain {
 
 	private static final Logger LOGGER = Logger.getGlobal();
+	
+	public static void main (String[] args){
+		run();
+	}
 
 	public static void run() {
 		LoggerConfigurator.configGlobal();
