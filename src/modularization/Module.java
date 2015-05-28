b2df06f82ba6405460e458682ece4ddd3cd5c8a3
Marcel Boeing <mboeing3@uni-koeln.de>
2015-05-28 09:08:23 +0200

    Removed unnecessary imports

--- src/modularization/Module.java
+++ src/modularization/Module.java
@@ -1,10 +1,5 @@
 package modularization;
 
-import java.io.IOException;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.PipedReader;
-import java.io.PipedWriter;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
