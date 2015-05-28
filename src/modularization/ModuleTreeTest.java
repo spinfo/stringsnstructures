f7a48dd324ee2402fed8eb3f8eb1eccb79ad0127
Marcel Boeing <mboeing3@uni-koeln.de>
2015-05-28 11:53:08 +0200

    Renamed variable

--- src/modularization/ModuleTreeTest.java
+++ src/modularization/ModuleTreeTest.java
@@ -21,22 +21,22 @@ public class ModuleTreeTest {
 		String outputFileLocation = System.getProperty("java.io.tmpdir")+File.separator+"test.txt";
 		
 		// Set up module tree
-		ModuleTree mc1 = new ModuleTree();
+		ModuleTree moduleTree = new ModuleTree();
 		
 		// Prepare OANC module
 		Properties oancProperties = new Properties();
 		oancProperties.setProperty(ModuleImpl.PROPERTYKEY_NAME, "OANC");
 		oancProperties.setProperty(OANC.PROPERTYKEY_OANCLOCATION, oancLoc0);
 		//oancProperties.setProperty(OANC.PROPERTYKEY_OANCLOCATION+"1", oancLoc1);
-		OANC oanc = new OANC(mc1,oancProperties);
+		OANC oanc = new OANC(moduleTree,oancProperties);
 		
-		mc1.setRootModule(oanc); // Necessary before adding more modules!
+		moduleTree.setRootModule(oanc); // Necessary before adding more modules!
 		
 		// Prepare FileWriter module
 		Properties fileWriterProperties = new Properties();
 		fileWriterProperties.setProperty(ModuleImpl.PROPERTYKEY_NAME, "FileWriter");
 		fileWriterProperties.setProperty(FileWriterModule.PROPERTYKEY_OUTPUTFILE, outputFileLocation);
-		FileWriterModule fileWriter = new FileWriterModule(mc1,fileWriterProperties);
+		FileWriterModule fileWriter = new FileWriterModule(moduleTree,fileWriterProperties);
 		
 		// Prepare OANC parser module
 		Properties oancParserProperties = new Properties();
@@ -46,31 +46,31 @@ public class ModuleTreeTest {
 		oancParserProperties.setProperty(OANCXMLParser.PROPERTYKEY_CONVERTTOLOWERCASE, Boolean.toString(true));
 		oancParserProperties.setProperty(OANCXMLParser.PROPERTYKEY_KEEPPUNCTUATION, Boolean.toString(true));
 		oancParserProperties.setProperty(OANCXMLParser.PROPERTYKEY_OUTPUTANNOTATEDJSON, Boolean.toString(true));
-		OANCXMLParser oancParser = new OANCXMLParser(mc1,oancParserProperties);
+		OANCXMLParser oancParser = new OANCXMLParser(moduleTree,oancParserProperties);
 		
 		// Prepare FileReader module
 		Properties fileReaderProperties = new Properties();
 		fileReaderProperties.setProperty(ModuleImpl.PROPERTYKEY_NAME, "FileReader");
 		fileReaderProperties.setProperty(FileReaderModule.PROPERTYKEY_INPUTFILE, outputFileLocation);
-		FileReaderModule fileReader = new FileReaderModule(mc1,fileReaderProperties);
+		FileReaderModule fileReader = new FileReaderModule(moduleTree,fileReaderProperties);
 		
 		// Prepare ConsoleWriter module
 		Properties consoleWriterProperties = new Properties();
 		consoleWriterProperties.setProperty(ModuleImpl.PROPERTYKEY_NAME, "ConsoleWriter");
-		ConsoleWriterModule consoleWriter = new ConsoleWriterModule(mc1,consoleWriterProperties);
+		ConsoleWriterModule consoleWriter = new ConsoleWriterModule(moduleTree,consoleWriterProperties);
 		
 		// Add modules to tree
-		mc1.addModule(consoleWriter, oanc);
-		mc1.addModule(oancParser, oanc);
-		mc1.addModule(fileWriter, oancParser);
+		moduleTree.addModule(consoleWriter, oanc);
+		moduleTree.addModule(oancParser, oanc);
+		moduleTree.addModule(fileWriter, oancParser);
 		
 		// Print chain
-		System.out.println(mc1.prettyPrint());
+		System.out.println(moduleTree.prettyPrint());
 		
 		
 		// run chain #1
 		System.out.println("Attempting to run chain #1");
-		mc1.runModules();
+		moduleTree.runModules();
 		
 		assertTrue(true);
 	}
