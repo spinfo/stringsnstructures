eee143d39a79223338280b56daf9abf3269c8f2d
Marcel Boeing <mboeing3@uni-koeln.de>
2015-06-08 09:08:53 +0200

    Removed unused import.

--- src/modularization/workbench/ModuleTreeCellRenderer.java
+++ src/modularization/workbench/ModuleTreeCellRenderer.java
@@ -5,8 +5,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
