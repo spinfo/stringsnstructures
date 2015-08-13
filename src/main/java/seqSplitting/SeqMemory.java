d7d7381f00ab0cd73d872a0bc323acc73f0ed653
christopher <c.kraus@uni-koeln.de>
2015-08-13 15:46:24 +0200

    - added properties description to the module -

--- src/main/java/seqSplitting/SeqMemory.java
+++ src/main/java/seqSplitting/SeqMemory.java
@@ -46,7 +46,8 @@ public class SeqMemory extends modularization.ModuleImpl {
 		// Define I/O
 		this.getSupportedInputs().add(CharPipe.class);
 		this.getSupportedOutputs().add(CharPipe.class);
-				
+		// Add module description
+		this.setDescription("Splits a DNA sequences (or any other continuous string) into a defined number of fragments with random length.");		
 	}
 	//end constructors
 	
