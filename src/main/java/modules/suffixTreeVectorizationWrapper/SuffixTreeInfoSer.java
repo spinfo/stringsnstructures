a69c27bad284ebe553fe1125e48990da37f8ed9b
christopher <c.kraus@uni-koeln.de>
2016-02-03 14:58:33 +0100

    new serializable class for vector generation

---
+++ src/main/java/modules/suffixTreeVectorizationWrapper/SuffixTreeInfoSer.java
@@ -0,0 +1,17 @@
+package modules.suffixTreeVectorizationWrapper;
+
+import modules.suffixTreeClustering.st_interface.SuffixTreeInfo;
+import java.io.Serializable;
+
+public class SuffixTreeInfoSer extends SuffixTreeInfo implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SuffixTreeInfoSer () {
+		super();
+	}
+
+}
