abb3f2cee3dc71c947ad0cb3930c468cc97efea5
Todor1987 <todorov87@gmail.com>
2016-06-08 22:38:59 +0200

    Improved CSV reading, still some Problems with hamming distance

--- src/main/java/modules/HammingDistance.java
+++ src/main/java/modules/HammingDistance.java
@@ -11,34 +11,53 @@ public class HammingDistance {
 	public static void main(String[] args) throws IOException {
 
 		 Scanner scanner = new Scanner(new File("/Users/TodorTodorov/morpho-out.csv"));
-	        scanner.useDelimiter(";");
+		 
+	        scanner.useDelimiter("\\n");
+	        
 			List<String> row = new ArrayList<String>();
+			
 			int i = 0;
+			
 			int distance = 0;
+			
 			while(scanner.hasNext()){
 	        	
 	            row.add(scanner.next());
 	            
 	            
-	            System.out.print(row.get(i) + " ");
+	            System.out.print(row.get(i) + " " + "\n");
 
 	            i++;
 	            
 	        }
+			row.remove(0);
 			
 			for(int i1= 0 ; i1 < row.size()-1; i1++){
-                for(int k = i1+1 ; k < row.size() ; k++){
-                    if(row.get(i1) == row.get(k)){
-                        System.out.println(i1 + "and" + k + "are pairs");
+				
+				//System.out.println(row.get(i1));
+				
+                for(int k = i1+1 ; k < row.size(); k++){
+                	
+                    if(row.get(i1) != row.get(k)){
+                    	
+                        distance++;
+
                     }
+
+                    
                 }
-			
+                
+                System.out.println(row.get(i1) + "  " + distance);
+
 	        
-	        scanner.close();
 	        
 	     
 	         
 	        
-			}}
+			}
+	        scanner.close();
+
+	
+	}
 
 }
