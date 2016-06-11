49a4e4982570cc5254ec0265366567b528caf56b
Todor1987 <todorov87@gmail.com>
2016-06-11 10:35:31 +0200

    Fixed

--- src/main/java/modules/HammingDistance.java
+++ src/main/java/modules/HammingDistance.java
@@ -15,46 +15,36 @@ public class HammingDistance {
 	        scanner.useDelimiter("\\n");
 	        
 			List<String> row = new ArrayList<String>();
-			
-			int i = 0;
-			
 			int distance = 0;
 			
 			while(scanner.hasNext()){
-	        	
-	            row.add(scanner.next());
-	            
-	            
-	            System.out.print(row.get(i) + " " + "\n");
-
-	            i++;
-	            
+				
+	            row.add(scanner.next());	            
+	            	            //System.out.print(row.get(i) + " " + "\n");
 	        }
-			row.remove(0);
+			//row.remove(0);
 			
-			for(int i1= 0 ; i1 < row.size()-1; i1++){
+			
+			String[] first;
+			String[] second;
+			System.out.println(row.get(0));
+
+			for (int j = 1; j < row.size() -1; j++) {
+				distance = 0;
 				
-				//System.out.println(row.get(i1));
+				first = row.get(j).split(";", -1);
+				second = row.get(j + 1).split(";", -1);
 				
-                for(int k = i1+1 ; k < row.size(); k++){
-                	
-                    if(row.get(i1) != row.get(k)){
-                    	
-                        distance++;
-
-                    }
-
-                    
-                }
-                
-                System.out.println(row.get(i1) + "  " + distance);
-
-	        
-	        
-	     
-	         
-	        
+				for(int k = 1; k < first.length; k++) {
+					if (!first[k].equals(second[k])) {
+						distance++;
+					}
+				}
+				
+				System.out.println(distance + "  "  + row.get(j));
 			}
+			System.out.println("   " + row.get(row.size()-1));
+    
 	        scanner.close();
 
 	
