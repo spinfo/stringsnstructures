a7a5b7df1df8727a02c34e776a3974db3d40fb5e
Marcel Boeing <mboeing3@uni-koeln.de>
2016-06-11 11:19:55 +0200

    Added parameter to specify power for Minkowski distance calculation.

--- src/main/java/common/VectorCalculation.java
+++ src/main/java/common/VectorCalculation.java
@@ -1,9 +1,7 @@
 package common;
 
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Provides vector calculation methods.
@@ -13,7 +11,7 @@ import java.util.Set;
 public class VectorCalculation {
 	
 	/**
-	 * Calculates the Minkowski-Distance of two n-dimensional vectors.
+	 * Calculates the (euclidian) Minkowski-Distance of two n-dimensional vectors.
 	 * Also refer to MERKL, Rainer 2015, Bioinformatik, p.159
 	 * and the <a href="https://en.wikipedia.org/wiki/Minkowski_distance">Wikipedia page</a> about the Minkowski-Distance.
 	 * @param vectorA First vector
@@ -21,7 +19,21 @@ public class VectorCalculation {
 	 * @return Minkowski-Distance
 	 * @throws Exception Thrown if vectors are null or of different length
 	 */
-	public static double calculateMinkowskiDistance(List<Double> vectorA, List<Double> vectorB) throws Exception{
+	public static double calculateMinkowskiDistance(List<Double> vectorA, List<Double> vectorB) throws Exception{		
+		return calculateMinkowskiDistance(vectorA, vectorB, 2d);
+	}
+	
+	/**
+	 * Calculates the Minkowski-Distance of two n-dimensional vectors.
+	 * Also refer to MERKL, Rainer 2015, Bioinformatik, p.159
+	 * and the <a href="https://en.wikipedia.org/wiki/Minkowski_distance">Wikipedia page</a> about the Minkowski-Distance.
+	 * @param vectorA First vector
+	 * @param vectorB Second vector
+	 * @param power 1: Hamming distance; 2: Euclidian; or any other positive value ...
+	 * @return Minkowski-Distance
+	 * @throws Exception Thrown if vectors are null or of different length
+	 */
+	public static double calculateMinkowskiDistance(List<Double> vectorA, List<Double> vectorB, double power) throws Exception{
 		
 		// Check input
 		if (vectorA==null || vectorB==null || vectorA.size()!=vectorB.size()){
@@ -35,7 +47,7 @@ public class VectorCalculation {
 		Iterator<Double> aIterator = vectorA.iterator();
 		Iterator<Double> bIterator = vectorB.iterator();
 		while(aIterator.hasNext() && bIterator.hasNext()){
-			result += Math.pow(Math.abs(aIterator.next()-bIterator.next()),2d);
+			result += Math.pow(Math.abs(aIterator.next()-bIterator.next()),power);
 		}
 		result = Math.sqrt(result);
 		
