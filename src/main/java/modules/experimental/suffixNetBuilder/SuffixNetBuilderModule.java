6f01e1337edebcd031551d528563d340462a8e9d
Marcel Boeing <mboeing3@uni-koeln.de>
2016-08-30 09:14:29 +0200

    Continued working on suffix net builder.

--- src/main/java/modules/experimental/suffixNetBuilder/SuffixNetBuilderModule.java
+++ src/main/java/modules/experimental/suffixNetBuilder/SuffixNetBuilderModule.java
@@ -1,61 +1,62 @@
 package modules.experimental.suffixNetBuilder;
 
-import java.io.BufferedReader;
-import java.io.ObjectOutputStream;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Scanner;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import common.parallelization.CallbackReceiver;
 import models.ExtensibleTreeNode;
-import modules.BytePipe;
 import modules.CharPipe;
 import modules.InputPort;
 import modules.ModuleImpl;
 import modules.OutputPort;
-import modules.Pipe;
-import modules.parser.oanc.WortAnnotationTupel;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import common.parallelization.CallbackReceiver;
 
 public class SuffixNetBuilderModule extends ModuleImpl {
-	
-	public static final String BERUEHRUNGSZAEHLER_SCHLUESSEL = "zaehler";
+
+	public static final String NODECOUNTER_KEY = "nodeCounter";
 	// Property keys
+	public static final String PROPERTYKEY_OUTERINPUTDELIMITER = "Input delimiter (outer)";
+	public static final String PROPERTYKEY_INNERINPUTDELIMITER = "Input delimiter (inner)";
 	public static final String PROPERTYKEY_INDIVIDUALBRANCHES = "Create individual branches";
-	public static final String PROPERTYKEY_INDIVIDUALROOTNODES = "Create individual rootnodes";
-	
+
 	// Local variables
 	private boolean individualBranches;
-	//private boolean individualRootNodes;
+	private String outerInputDelimiter = "\\$";
+	private String innerInputDelimiter = "[\\s]+";
 	private final String INPUTID = "input";
 	private final String OUTPUTID = "output";
 
-	public SuffixNetBuilderModule(CallbackReceiver callbackReceiver,
-			Properties properties) throws Exception {
+	public SuffixNetBuilderModule(CallbackReceiver callbackReceiver, Properties properties) throws Exception {
 		super(callbackReceiver, properties);
 
 		// Define I/O
-		InputPort inputPort = new InputPort(INPUTID, "JSON-encoded FileFinderModule-data, with the parsed contents of one source file per line.", this);
+		InputPort inputPort = new InputPort(INPUTID, "Plain text input.", this);
 		inputPort.addSupportedPipe(CharPipe.class);
-		OutputPort outputPort = new OutputPort(OUTPUTID, "Suffix net in binary object form.", this);
-		outputPort.addSupportedPipe(BytePipe.class);
+		OutputPort outputPort = new OutputPort(OUTPUTID, "JSON encoded graph.", this);
+		outputPort.addSupportedPipe(CharPipe.class);
 		super.addInputPort(inputPort);
 		super.addOutputPort(outputPort);
-		
+
 		// Add description for properties
-		this.getPropertyDescriptions().put(PROPERTYKEY_INDIVIDUALBRANCHES, "Creates individual branches for each sentence.");
-		this.getPropertyDescriptions().put(PROPERTYKEY_INDIVIDUALROOTNODES, "Creates individual root nodes for each sentence.");
+		this.getPropertyDescriptions().put(PROPERTYKEY_INDIVIDUALBRANCHES,
+				"Creates individual branches for each sentence.");
+		this.getPropertyDescriptions().put(PROPERTYKEY_OUTERINPUTDELIMITER,
+				"<p>The <i>outer input delimiter</i> is used to discern strings from each other that will be inserted into the resulting graph independently, resulting into a <i>Generalised Suffix Net</i>.</p><p>The value is interpreted as a <i>Regular Expression</i>, e.g. '$' marks the end of a line and '\\$' means the actual dollar sign; set to '\\z' for single string input.</p>");
+		this.getPropertyDescriptions().put(PROPERTYKEY_INNERINPUTDELIMITER,
+				"Regular expression to use as inner segmentation delimiter for the input; leave empty for char-by-char segmentation.");
 
 		// Add default values
 		this.getPropertyDefaultValues().put(ModuleImpl.PROPERTYKEY_NAME, "SuffixNetBuilder");
 		this.getPropertyDefaultValues().put(PROPERTYKEY_INDIVIDUALBRANCHES, "false");
-		this.getPropertyDefaultValues().put(PROPERTYKEY_INDIVIDUALROOTNODES, "false");
+		this.getPropertyDefaultValues().put(PROPERTYKEY_INNERINPUTDELIMITER, "\\s+");
+		this.getPropertyDefaultValues().put(PROPERTYKEY_OUTERINPUTDELIMITER, "\\R+");
 
 		// Add module description
-		this.setDescription("Builds a suffix net from the annotated JSON output of OANCXMLParser (expects one JSON object per line).");
+		this.setDescription("Builds a suffix net (for want of a better name) from the text input.");
 
 	}
 
@@ -67,83 +68,79 @@ public class SuffixNetBuilderModule extends ModuleImpl {
 	@Override
 	public void applyProperties() throws Exception {
 		super.setDefaultsIfMissing();
+		this.outerInputDelimiter = this.getProperties().getProperty(PROPERTYKEY_OUTERINPUTDELIMITER,
+				this.getPropertyDefaultValues().get(PROPERTYKEY_OUTERINPUTDELIMITER));
+		this.innerInputDelimiter = this.getProperties().getProperty(PROPERTYKEY_INNERINPUTDELIMITER,
+				this.getPropertyDefaultValues().get(PROPERTYKEY_INNERINPUTDELIMITER));
 		if (this.getProperties().containsKey(PROPERTYKEY_INDIVIDUALBRANCHES))
-			this.individualBranches = Boolean.parseBoolean(this.getProperties().getProperty(PROPERTYKEY_INDIVIDUALBRANCHES));
-		//if (this.getProperties().containsKey(PROPERTYKEY_INDIVIDUALROOTNODES))
-			//this.individualRootNodes = Boolean.parseBoolean(this.getProperties().getProperty(PROPERTYKEY_INDIVIDUALROOTNODES));
+			this.individualBranches = Boolean
+					.parseBoolean(this.getProperties().getProperty(PROPERTYKEY_INDIVIDUALBRANCHES));
 		super.applyProperties();
 	}
 
 	@Override
 	public boolean process() throws Exception {
 
-		// Wurzelknoten des zu erstellenden Baumes erstellen
-		ExtensibleTreeNode wurzelKnoten = new ExtensibleTreeNode("^");
+		// Create the suffix net root node
+		ExtensibleTreeNode rootNode = new ExtensibleTreeNode("^");
 
-		// JSON-Parser instanziieren
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		
 		// Variable zur Nachhaltung erstellter Graphenknoten
 		Map<String, ExtensibleTreeNode> createdNodes = null;
 		if (!this.individualBranches)
 			createdNodes = new HashMap<String, ExtensibleTreeNode>();
 
-		// Eingabe puffern
-		BufferedReader eingabe = new BufferedReader(this.getInputPorts().get(INPUTID).getInputReader());
+		// Instantiate outer input scanner
+		Scanner outerInputScanner = new Scanner(this.getInputPorts().get(INPUTID).getInputReader());
+		outerInputScanner.useDelimiter(this.outerInputDelimiter);
 
-		// Eingabe einlesen
-		String jsonObjekt = eingabe.readLine();
-		while (jsonObjekt != null) {
+		// Outer input read loop
+		while (outerInputScanner.hasNext()) {
 
-			// JSON-Objekt parsen
-			WortAnnotationTupel[][] saetze = gson.fromJson(jsonObjekt,
-					new WortAnnotationTupel[0][0].getClass());
-
-			// Saetze durchlaufen
-			for (int i = 0; i < saetze.length; i++) {
-
-				WortAnnotationTupel[] satz = saetze[i];
-
-				// Satz in String-Array fassen
-				String[] satzArray = new String[satz.length];
-				for (int j = 0; j < satz.length; j++) {
-					satzArray[j] = satz[j].getWort();
-				}
-
-				// Mit dem ermittelten Satz wird das Netz weiter konstruiert
-				this.buildSuffixNet(satzArray, wurzelKnoten, createdNodes);
-
+			// Check for interrupt signal
+			if (Thread.interrupted()) {
+				outerInputScanner.close();
+				this.closeAllOutputs();
+				throw new InterruptedException("Thread has been interrupted.");
 			}
 
-			// Naechstes Objekt einlesen
-			jsonObjekt = eingabe.readLine();
+			// Continue building the suffix net
+			this.buildSuffixNet(outerInputScanner.next().split(this.innerInputDelimiter), rootNode, createdNodes);
 
 		}
 
-		// Letztlich wird der Wurzelknoten (und damit der gesamte erstellte
-		// Baum) als Bytestrom ausgegeben
-		Iterator<Pipe> byteStroeme = this.getOutputPorts().get(OUTPUTID).getPipes(BytePipe.class).iterator();
-		while (byteStroeme.hasNext()){
-			ObjectOutputStream ausgabeStrom = new ObjectOutputStream(((BytePipe)byteStroeme.next()).getOutput());
-			ausgabeStrom.writeObject(wurzelKnoten);
-			ausgabeStrom.close();
-		}
+		// Close input scanner
+		outerInputScanner.close();
 
-		// Ausgabekanaele schliessen
+		// Initialise JSON serialiser
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		Gson gson = gsonBuilder.setPrettyPrinting().create();
+
+		// Create JSON
+		String json = gson.toJson(rootNode);
+
+		// Write to outputs
+		this.getOutputPorts().get(OUTPUTID).outputToAllCharPipes(json);
+
+		// Close outputs
 		this.closeAllOutputs();
 
 		return true;
 	}
 
 	/**
-	 * Builds a suffix net based on the given root node and writes it into a Neo4j-database (via given client).
-	 * @param token Token of strings (will be nodes within the resulting net)
-	 * @param rootnode Node/URI to start from
-	 * @param graph Neo4J-REST-client
-	 * @param createdNodes Map used to keep track of created nodes. If null, each call will create a different individual branch.
+	 * Builds a suffix net based on the given root node.
+	 * 
+	 * @param token
+	 *            Token of strings (will be nodes within the resulting net)
+	 * @param rootnode
+	 *            Node to start from
+	 * @param createdNodes
+	 *            Map used to keep track of created nodes. If null, each call
+	 *            will create a different individual branch.
 	 * @throws Exception
 	 */
-	private void buildSuffixNet(String[] token, ExtensibleTreeNode rootnode, Map<String, ExtensibleTreeNode> createdNodes) throws Exception {
+	private void buildSuffixNet(String[] token, ExtensibleTreeNode rootnode,
+			Map<String, ExtensibleTreeNode> createdNodes) throws Exception {
 
 		// Keep track of created nodes
 		if (createdNodes == null)
@@ -164,12 +161,13 @@ public class SuffixNetBuilderModule extends ModuleImpl {
 				// If so, retrieve the node
 				child = createdNodes.get(token[i]);
 
-				// Check whether that node is already connected to the current parent node
-				if (!parentNode.getChildNodes().containsKey(token[i])){
-					
+				// Check whether that node is already connected to the current
+				// parent node
+				if (!parentNode.getChildNodes().containsKey(token[i])) {
+
 					// If not, make the connection
 					parentNode.getChildNodes().put(token[i], child);
-					
+
 				}
 			}
 
@@ -180,14 +178,14 @@ public class SuffixNetBuilderModule extends ModuleImpl {
 
 				// Connect it to parent
 				parentNode.getChildNodes().put(token[i], child);
-				
+
 				// Remember it as having been created
 				createdNodes.put(token[i], child);
 			}
 
 			// Increment the counter of the child node
-			child.setNodeCounter(child.getNodeCounter()+1);
-			
+			child.setNodeCounter(child.getNodeCounter() + 1);
+
 			// Set new parent
 			parentNode = child;
 		}
