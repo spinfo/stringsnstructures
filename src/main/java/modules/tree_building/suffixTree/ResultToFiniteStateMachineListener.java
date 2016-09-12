adcf6d970c5817821755cae16759d5c50a418808
David Neugebauer <david.neugebauer@posteo.de>
2016-09-12 11:47:50 +0200

    Let the FiniteStateMachineListener keep a view of States ordered by their distance to a leaf.

--- src/main/java/modules/tree_building/suffixTree/ResultToFiniteStateMachineListener.java
+++ src/main/java/modules/tree_building/suffixTree/ResultToFiniteStateMachineListener.java
@@ -1,8 +1,11 @@
 package modules.tree_building.suffixTree;
 
 import java.io.IOException;
-import java.util.ListIterator;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.Stack;
+import java.util.TreeMap;
 
 import modules.transitionNetwork.TransitionNetwork;
 import modules.transitionNetwork.elements.StateElement;
@@ -27,24 +30,30 @@ public class ResultToFiniteStateMachineListener implements ITreeWalkerListener {
 	// the length of the path currently read
 	private int lengthOfPath;
 
+	// keep StateElements ordered by their distance to a leaf
+	private Map<Integer, Set<StateElement>> stateLeafDistanceView = new TreeMap<>();
+
 	public ResultToFiniteStateMachineListener(BaseSuffixTree suffixTree, boolean inverted) {
 		this.tree = suffixTree;
 		this.nodeNrs = new Stack<Integer>();
 		this.inverted = inverted;
 		this.tn = new TransitionNetwork(suffixTree.text, this.inverted);
 		this.nodeNrsOfLastFullPath = new Stack<Integer>();
+		
+		// set the root state of the network to the root node id of the tree
+		this.tn.setRootStateId(this.tree.getRoot());
 	}
 
 	public ResultToFiniteStateMachineListener(BaseSuffixTree suffixTree) {
 		this(suffixTree, true);
 	}
 
-	public void setTN(TransitionNetwork tn) {
-		this.tn = tn;
-	}
-
 	public TransitionNetwork getTN() {
 		return this.tn;
+	}
+
+	public Map<Integer, Set<StateElement>> getStateLeafDistanceView() {
+		return this.stateLeafDistanceView;
 	}
 
 	public void setInverted(boolean inverted) {
@@ -62,7 +71,7 @@ public class ResultToFiniteStateMachineListener implements ITreeWalkerListener {
 		// if the current node is a leaf of a whole input text, it gets
 		// processed and the the path to it is recorded in a separate stack
 		if (nodeIsLeafOfWholeInputText(nodeNr, this.lengthOfPath)) {
-			processLeavesOfInputTexts(nodeNr, level);
+			processLeafOfInputTexts(nodeNr, level);
 
 			this.nodeNrsOfLastFullPath.clear();
 			this.nodeNrsOfLastFullPath.addAll(nodeNrs);
@@ -77,38 +86,30 @@ public class ResultToFiniteStateMachineListener implements ITreeWalkerListener {
 		}
 	}
 
-	public void processLeavesOfInputTexts(int nodeNr, int level) throws IOException {
-
+	public void processLeafOfInputTexts(int leafNodeNr, int level) throws IOException {
 		// we need to enter the node stack (i.e. the path that leads to the
 		// current node) at that position where a backtrack into the last full
 		// path processed by this method occurred
-		ListIterator<Integer> it;
+		int nodeStackIdx;
 		if (this.nodeNrsOfLastFullPath.isEmpty()) {
-			it = this.nodeNrs.listIterator();
+			nodeStackIdx = 0;
 		} else {
-			it = this.nodeNrs.listIterator(nodeNrsOfLastFullPath.size() - 1);
-		}
-
-		// the index of the node in the Transition Network equals the node's
-		// nodeNr in the tree
-		int nodeIndex = 0;
-		if (it.hasNext()) {
-			nodeIndex = it.next();
-		} else {
-			throw new IllegalStateException("Node stack empty or not entered at correct position.");
+			nodeStackIdx = nodeNrsOfLastFullPath.size() - 1;
 		}
 
 		// loop over the remaining path and generate transition elements to
 		// model the transition from one node to another
-		while (it.hasNext()) {
+		for (; nodeStackIdx < nodeNrs.size() - 1; nodeStackIdx++) {
+			// the node number in the tree given by the stack
+			int nodeNr = this.nodeNrs.get(nodeStackIdx);
 
 			// get or insert the node's corresponding state element if
 			// it doesn't exist
-			int stateId = this.tn.addStateElement(new StateElement(nodeIndex));
+			int stateId = this.tn.addStateElement(new StateElement(nodeNr));
 			StateElement stateElement = this.tn.states.get(stateId);
 
-			// the node numer of the next node on the path
-			int childNodeIndex = it.next();
+			// the node number of the next node on the path
+			int childNodeNr = this.nodeNrs.get(nodeStackIdx + 1);
 
 			// model the transition to the next state
 			StateTransitionElement transition = new StateTransitionElement();
@@ -118,25 +119,32 @@ public class ResultToFiniteStateMachineListener implements ITreeWalkerListener {
 			// definition of this method), model it as a transition to the
 			// network's final state instead
 			int childStateId;
-			if (it.hasNext()) {
-				childStateId = this.tn.addStateElement(new StateElement(childNodeIndex));
-			} else {
+			if (childNodeNr == leafNodeNr) {
 				childStateId = this.tn.getFinalStateId();
+			} else {
+				childStateId = this.tn.addStateElement(new StateElement(childNodeNr));
 			}
 			transition.toStateElement = childStateId;
 			stateElement.toStateTransitions.add(transition);
 
 			// generate Suffix Element, add it to the network and link to it
 			// from the transition
-			int suffixStart = this.tree.nodes[childNodeIndex].getStart(0);
-			int suffixEnd = this.tree.nodes[childNodeIndex].getEnd(0);
+			int suffixStart = this.tree.nodes[childNodeNr].getStart(0);
+			int suffixEnd = this.tree.nodes[childNodeNr].getEnd(0);
 			SuffixElement suffixElement = new SuffixElement(suffixStart, suffixEnd);
 			int posInSuffixes = this.tn.addSuffixElement(suffixElement);
 			transition.toSuffixElement = posInSuffixes;
 
-			// this loop's child will be next loop's parent
-			nodeIndex = childNodeIndex;
+			// register the new state, it's distance to the leaf being the
+			// remaining nodes on the stack
+			registerStateByLeafDistance(stateElement, (this.nodeNrs.size() - nodeStackIdx - 1));
 		}
+	}
+
+	private void registerStateByLeafDistance(StateElement state, int distance) {
+		Set<StateElement> states = this.stateLeafDistanceView.getOrDefault(distance, new HashSet<StateElement>());
+		states.add(state);
+		this.stateLeafDistanceView.put(distance, states);
 	}
 
 	// checks if the given node in this listeners tree corresponds to a whole
