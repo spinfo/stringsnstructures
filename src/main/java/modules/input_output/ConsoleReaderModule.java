3d5a175d2b7617f411abdd723657eeef24c404ae
Marcel Boeing <mboeing3@uni-koeln.de>
2016-10-14 14:16:33 +0200

    Fixed Console Reader not working with standalone execution modules.

--- src/main/java/modules/input_output/ConsoleReaderModule.java
+++ src/main/java/modules/input_output/ConsoleReaderModule.java
@@ -1,13 +1,11 @@
 package modules.input_output;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Properties;
 
 import common.parallelization.CallbackReceiver;
-import modules.BytePipe;
 import modules.CharPipe;
 import modules.ModuleImpl;
 import modules.OutputPort;
@@ -28,9 +26,8 @@ public class ConsoleReaderModule extends ModuleImpl {
 		super(callbackReceiver, properties);
 
 		// set I/O -- no inputs allowed here (we'll read from stdin)
-		OutputPort outputPort = new OutputPort(OUTPUTID, "Byte or character output.", this);
+		OutputPort outputPort = new OutputPort(OUTPUTID, "Character output.", this);
 		outputPort.addSupportedPipe(CharPipe.class);
-		outputPort.addSupportedPipe(BytePipe.class);
 		super.addOutputPort(outputPort);
 
 		// Add description for properties
@@ -47,96 +44,47 @@ public class ConsoleReaderModule extends ModuleImpl {
 		this.getPropertyDefaultValues().put(PROPERTYKEY_BUFFERLENGTH, "8192");
 
 		// Add module description
-		this.setDescription("Reads contents from stdin.");
+		this.setDescription("Reads contents from stdin and relays it via character stream.");
 
 	}
 
 	@Override
 	public boolean process() throws Exception {
 
-		/*
-		 * write to both output channels (stream/writer)
-		 */
-		boolean wroteToStream = false;
-		try {
-			// Instantiate a new input stream
-			InputStream inputStream = System.in;
+		// Instantiate a new input stream
+		InputStream inputStream = System.in;
+
+		// Instantiate input reader if an encoding has been set
+		Reader reader = null;
+		if (this.encoding != null && !this.encoding.isEmpty()) {
+			reader = new InputStreamReader(inputStream, encoding);
 
 			// Define input buffer
-			byte[] buffer = new byte[this.bufferLength];
+			char[] buffer = new char[this.bufferLength];
 
-			// Read file data into buffer and write to outputstream
-			int readBytes = inputStream.read(buffer);
-			while (readBytes != -1) {
+			// Read file data into buffer and output to writer
+			int readChars = reader.read(buffer);
+			while (readChars != -1) {
 
 				// Auf Unterbrechersignal pruefen
 				if (Thread.interrupted()) {
+					reader.close();
 					inputStream.close();
 					this.closeAllOutputs();
-					throw new InterruptedException(
-							"Thread has been interrupted.");
+					throw new InterruptedException("Thread has been interrupted.");
 				}
-
-				this.getOutputPorts().get(OUTPUTID).outputToAllBytePipes(buffer, 0, readBytes);
-				readBytes = inputStream.read(buffer);
+				this.getOutputPorts().get(OUTPUTID).outputToAllCharPipes(buffer, 0, readChars);
+				readChars = reader.read(buffer);
 			}
 
 			// close relevant I/O instances
-			inputStream.close();
-			wroteToStream = true;
-		} catch (IOException e) {
-			/*
-			 * The inputstream does not seem to be connected or another
-			 * I/O-error occurred
-			 */
+			reader.close();
 		}
 
-		boolean wroteToChars = false;
-		try {
-			// Instantiate a new input stream
-			InputStream inputStream = System.in;
+		// close relevant I/O instances
+		inputStream.close();
 
-			// Instantiate input reader if an encoding has been set
-			Reader reader = null;
-			if (this.encoding != null && !this.encoding.isEmpty()) {
-				reader = new InputStreamReader(inputStream, encoding);
-
-				// Define input buffer
-				char[] buffer = new char[this.bufferLength];
-
-				// Read file data into buffer and output to writer
-				int readChars = reader.read(buffer);
-				while (readChars != -1) {
-
-					// Auf Unterbrechersignal pruefen
-					if (Thread.interrupted()) {
-						reader.close();
-						inputStream.close();
-						this.closeAllOutputs();
-						throw new InterruptedException(
-								"Thread has been interrupted.");
-					}
-					this.getOutputPorts().get(OUTPUTID).outputToAllCharPipes(buffer, 0, readChars);
-					readChars = reader.read(buffer);
-				}
-
-				// close relevant I/O instances
-				reader.close();
-			}
-
-			// close relevant I/O instances
-			inputStream.close();
-		} catch (IOException e) {
-			/*
-			 * The inputstream does not seem to be connected or another
-			 * I/O-error occurred
-			 */
-		}
-		
 		this.getOutputPorts().get(OUTPUTID).close();
-
-		if (!wroteToStream && !wroteToChars)
-			throw new Exception("Sorry, but I could not write to any output (please connect a module to my output, else I am of little use).");
 
 		// Success
 		return true;
