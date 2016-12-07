040c18a98d502f42aca24cc457c0c684c0379e82
Marcel Boeing <mboeing3@uni-koeln.de>
2016-12-07 09:38:46 +0100

    Added search filter for module template list.

--- src/main/java/base/workbench/ModuleJTreeCellRenderer.java
+++ src/main/java/base/workbench/ModuleJTreeCellRenderer.java
@@ -1,10 +1,13 @@
 package base.workbench;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.util.Enumeration;
 
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeNode;
 
 import modules.Module;
 
@@ -17,24 +20,96 @@ public class ModuleJTreeCellRenderer extends DefaultTreeCellRenderer {
 
 	private static final long serialVersionUID = 8321180041458513120L;
 	
+	// Filter string
+	private String filterString;
+	
+	public String getFilterString() {
+		return filterString;
+	}
+
+	public void setFilterString(String filterString) {
+		if (filterString != null)
+			this.filterString = filterString.toLowerCase();
+		else
+			this.filterString = null;
+	}
+
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value,
 			boolean sel, boolean expanded, boolean leaf, int row,
 			boolean hasFocus) {
-		// Determine whether a node carrying a module is selected
-		if (value != null
-				&& DefaultMutableTreeNode.class.isAssignableFrom(value.getClass())
-				&& ((DefaultMutableTreeNode)value).getUserObject() != null
-				&& Module.class.isAssignableFrom(((DefaultMutableTreeNode)value).getUserObject().getClass())){
-			Module module = (Module)((DefaultMutableTreeNode)value).getUserObject();
-			// Set tooltip to selected module's description
-			setToolTipText("<html><div style='background-color:FFFFFF;' width=\"500\"><h1 style='font-size:12px;'>"+module.getName()+"</h1><p>"+module.getDescription()+"</p></div></html>");
-		} else
-			setToolTipText(null);
 		
 		// Call super method
-		return super.getTreeCellRendererComponent(tree, value, sel,
+		Component c = super.getTreeCellRendererComponent(tree, value, sel,
 				expanded, leaf, row, hasFocus);
+		
+		// Cast value to DefaultMutableTreeNode
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+        
+		// Determine whether a node carrying a module is selected
+		if (Module.class.isAssignableFrom(node.getUserObject().getClass())){
+			Module module = (Module)node.getUserObject();
+			// Set tooltip to selected module's description
+			setToolTipText("<html><div style='background-color:FFFFFF;' width=\"500\"><h1 style='font-size:12px;'>"+module.getName()+"</h1><p>"+module.getDescription()+"</p></div></html>");
+			// Colorise node according to it matching the filter string
+	        if (moduleNodeMatchesFilter(node)) {
+	            c.setForeground(Color.BLACK);
+	            return c;
+	        }
+	        else {
+	            c.setForeground(Color.GRAY);
+	            return c;
+	        }
+		} else{
+			setToolTipText(null);
+			// Colorise node according to it matching the filter string
+	        if (categoryNodeMatchesFilter(node)) {
+	            c.setForeground(Color.BLACK);
+	            return c;
+	        }
+	        else {
+	            c.setForeground(Color.GRAY);
+	            return c;
+	        }
+		}
+	}
+	
+	private boolean categoryNodeMatchesFilter(DefaultMutableTreeNode node) 
+	{
+		if (this.filterString == null || this.filterString.isEmpty())
+			return true;
+		
+	    if ( node.getUserObject().toString().toLowerCase().contains(this.filterString)) {
+            return true;
+        }
+		
+		@SuppressWarnings("unchecked")
+		Enumeration<TreeNode> childrenEnum = node.children();
+	    while ( childrenEnum.hasMoreElements() ) {
+	    	
+	    	DefaultMutableTreeNode child = (DefaultMutableTreeNode)childrenEnum.nextElement();
+	        
+	    	// Determine whether a node carrying a module is selected (and if so, search in module name & description)
+			if (child.getUserObject() != null
+					&& Module.class.isAssignableFrom(child.getUserObject().getClass()) && (((Module)child.getUserObject()).getName().toLowerCase().contains(filterString) || ((Module)child.getUserObject()).getDescription().toLowerCase().contains(filterString))){
+				return true;
+			}
+	    }
+	    
+	    return false;
+	}
+	
+	private boolean moduleNodeMatchesFilter(DefaultMutableTreeNode node) {
+		
+		if (this.filterString == null || this.filterString.isEmpty())
+			return true;
+		if (node.getUserObject() != null && Module.class.isAssignableFrom(node.getUserObject().getClass())
+				&& (((Module) node.getUserObject()).getName().toLowerCase().contains(filterString)
+						|| ((Module) node.getUserObject()).getDescription().toLowerCase().contains(filterString))) {
+			return true;
+		}
+
+		return false;
 	}
 
 }
