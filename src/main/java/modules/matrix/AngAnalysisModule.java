6d1c2200930ead4850044701b204a9b3fa4d8a15
bkis <kiss@mailbox.org>
2017-02-01 14:40:48 +0100

    update AngAnalysisModule

--- src/main/java/modules/matrix/AngAnalysisModule.java
+++ src/main/java/modules/matrix/AngAnalysisModule.java
@@ -32,6 +32,7 @@ public class AngAnalysisModule extends ModuleImpl {
 	// Identifiers for inputs and outputs
 	public static final String INPUT_ID_CORPUS = "input corpus";
 	public static final String INPUT_ID_TERMS = "input terms";
+	public static final String INPUT_ID_STOPWORDS = "input stop words";
 	public static final String OUTPUT_ID_MATRIX = "output matrix";
 	public static final String OUTPUT_ID_META = "output meta data";
 	
@@ -52,21 +53,24 @@ public class AngAnalysisModule extends ModuleImpl {
 		this.getPropertyDescriptions().put(PROPERTYKEY_DELIMITER, "CSV delimiter");
 		this.getPropertyDefaultValues().put(PROPERTYKEY_DELIMITER, ";");
 		this.getPropertyDescriptions().put(PROPERTYKEY_NMOSTFREQ, "use n most frequent co-occurrences (number)");
-		this.getPropertyDefaultValues().put(PROPERTYKEY_DELIMITER, "30");
+		this.getPropertyDefaultValues().put(PROPERTYKEY_NMOSTFREQ, "30");
 
 		// setup I/O
 		InputPort inputCorpus = new InputPort(INPUT_ID_CORPUS, "Input corpus in text form", this);
 		InputPort inputTerms = new InputPort(INPUT_ID_TERMS, "Input terms list, newline separated", this);
+		InputPort inputStopWords = new InputPort(INPUT_ID_STOPWORDS, "Input stop words file, newline separated, optional", this);
 		OutputPort outputMatrix = new OutputPort(OUTPUT_ID_MATRIX, "Matrix of non-normalized co-occurrence vectors [CSV]", this);
 		OutputPort outputMeta = new OutputPort(OUTPUT_ID_META, "Analysis meta data [plain text]", this);
 		
 		inputCorpus.addSupportedPipe(CharPipe.class);
 		inputTerms.addSupportedPipe(CharPipe.class);
+		inputStopWords.addSupportedPipe(CharPipe.class);
 		outputMatrix.addSupportedPipe(CharPipe.class);
 		outputMeta.addSupportedPipe(CharPipe.class);
 
 		this.addInputPort(inputCorpus);
 		this.addInputPort(inputTerms);
+		this.addInputPort(inputStopWords);
 		this.addOutputPort(outputMatrix);
 		this.addOutputPort(outputMeta);
 	}
@@ -87,7 +91,15 @@ public class AngAnalysisModule extends ModuleImpl {
 		Set<String> terms = new HashSet<String>();
 		reader = new BufferedReader(super.getInputPorts().get(INPUT_ID_TERMS).getInputReader());
 		while ((line = reader.readLine()) != null)
-			terms.add(line);
+			terms.add(line.toUpperCase()); //UPPERCASED
+		
+		//read stop words
+		Set<String> stopWords = new HashSet<String>();
+		if (super.getInputPorts().get(INPUT_ID_STOPWORDS).isConnected()){
+			reader = new BufferedReader(super.getInputPorts().get(INPUT_ID_STOPWORDS).getInputReader());
+			while ((line = reader.readLine()) != null)
+				stopWords.add(line.toUpperCase()); //UPPERCASED
+		}
 		
 		////read and process corpus
 		reader = new BufferedReader(super.getInputPorts().get(INPUT_ID_CORPUS).getInputReader());
@@ -97,6 +109,7 @@ public class AngAnalysisModule extends ModuleImpl {
 		
 		//process corpus documents
 		while ((line = reader.readLine()) != null){
+			line = cleanString(line, stopWords, 2); //filter stop words and remove tokens shorter than 2 chars
 			for (String t : terms){
 				List<String[]> contexts = trimTextMulti(line, t, contextWindow, false); //get context coOccs
 				countContexts.put(t, countContexts.get(t) + contexts.size()); //count contexts
@@ -107,6 +120,9 @@ public class AngAnalysisModule extends ModuleImpl {
 			}
 		}
 		
+		//cleanup
+		reader.close();
+		
 		//sort and trim coOcc maps
 		for (String key : coOccMap.keySet()){
 			sortMapByValue(coOccMap.get(key), false);
@@ -114,12 +130,30 @@ public class AngAnalysisModule extends ModuleImpl {
 		}
 		
 		//output matrix
-		outMatrix.outputToAllCharPipes(coOccurrenceMapToCSV(coOccMap, delimiter.charAt(0)));
+		outMatrix.outputToAllCharPipes(coOccurrenceMapToCSV(coOccMap, delimiter));
 		
 		//output meta
 		outMeta.outputToAllCharPipes(writeContextCountMap(countContexts));
 		
+		//cleanup
+		this.closeAllOutputs();
+		
 		return result;
+	}
+	
+	
+	private String cleanString(String s, Set<String> stopWords, int minLength){
+		String[] tokens = s.split("\\P{L}+");
+		StringBuilder sb = new StringBuilder();
+		
+		for (String t : tokens){
+			if (!stopWords.contains(t) && t.length() >= minLength){
+				sb.append(t);
+				sb.append(" ");
+			}
+		}
+		
+		return sb.toString().trim();
 	}
 	
 
@@ -148,6 +182,8 @@ public class AngAnalysisModule extends ModuleImpl {
 		for (String c : coOccs){
 			if (coOccs.equals(term))
 				continue;
+			if (coOccMap.get(term) == null)
+				coOccMap.put(term, new LinkedHashMap<String, Integer>());
 			if (coOccMap.get(term).get(c) == null)
 				coOccMap.get(term).put(c, 0);
 			//add
@@ -180,7 +216,7 @@ public class AngAnalysisModule extends ModuleImpl {
 	}
 	
 	
-	private String coOccurrenceMapToCSV(LinkedHashMap<String, LinkedHashMap<String, Integer>> map, char delimiter){
+	private String coOccurrenceMapToCSV(LinkedHashMap<String, LinkedHashMap<String, Integer>> map, String delimiter){
 		
 		////construct joined map
 		Map<String, Integer> coOccs = new LinkedHashMap<String, Integer>();
