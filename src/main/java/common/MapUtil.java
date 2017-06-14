23e2f53b3f44e9650a0bfacef894a9e0244ef63b
Peter Seipel <pseipel1@uni-koeln.de>
2017-06-14 15:48:27 +0200

    DistanceMatrixModule added static MapUtil Class added VectorMath Class with two distance calculations

---
+++ src/main/java/common/MapUtil.java
@@ -0,0 +1,31 @@
+package common;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MapUtil {
+
+	public static <K, V extends Comparable<? super V>> LinkedHashMap<K, V> sortMapByValue(Map<K, V> map, final boolean ascending) {
+		List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+			@Override
+			public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+				if (ascending)
+					return (o1.getValue()).compareTo(o2.getValue());
+				else
+					return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+
+		LinkedHashMap<K, V> result = new LinkedHashMap<K, V>();
+		for (Map.Entry<K, V> entry : list) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+		return result;
+	}
+	
+}
---
+++ src/main/java/modules/matrix/distanceModule/VectorMath.java
@@ -0,0 +1,26 @@
+package modules.matrix.distanceModule;
+import java.util.List;
+
+public class VectorMath {
+	
+	public static double euclidianDistance(double[] ds, double[] ds2) {
+        double diff_square_sum = 0.0;
+        for (int i = 0; i < ds.length; i++) {
+            diff_square_sum += (ds[i] - ds2[i]) * (ds[i] - ds2[i]);
+        }
+        return Math.sqrt(diff_square_sum);
+    }
+	
+	public static double cosineSimilarity(double[] vectorA, double[] vectorB) {
+	    double dotProduct = 0.0;
+	    double normA = 0.0;
+	    double normB = 0.0;
+	    for (int i = 0; i < vectorA.length; i++) {
+	        dotProduct += vectorA[i] * vectorB[i];
+	        normA += Math.pow(vectorA[i], 2);
+	        normB += Math.pow(vectorB[i], 2);
+	    }   
+	    return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+	}
+
+}
