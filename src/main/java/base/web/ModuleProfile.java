76917885366c70a2b97cdd2f53386be41e1dc76c
David Neugebauer <david.neugebauer@posteo.de>
2018-03-25 22:59:23 +0200

    Added expose annotations to module profile.

--- src/main/java/base/web/ModuleProfile.java
+++ src/main/java/base/web/ModuleProfile.java
@@ -5,46 +5,58 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.google.gson.annotations.Expose;
+
 import modules.Module;
 import modules.Port;
 
 class ModuleProfile {
 
+	@Expose
 	String name;
-	
+
+	@Expose
 	String canonicalClassName;
-	
+
+	@Expose
 	String description;
-	
+
+	@Expose
 	String category;
-	
+
+	@Expose
 	Map<String, String> propertyDescriptions;
-	
+
+	@Expose
 	Map<String, String> propertyDefaultValues;
-	
+
+	@Expose
 	List<PortProfile> inputPorts;
-	
+
+	@Expose
 	List<PortProfile> outputPorts;
-	
+
 	class PortProfile {
-		
+
+		@Expose
 		String name;
-		
+
+		@Expose
 		String description;
-		
+
+		@Expose
 		Set<String> supportedPipes;
-		
+
 		PortProfile(Port port) {
 			this.name = port.getName();
 			this.description = port.getDescription();
-			
-			this.supportedPipes = port.getSupportedPipeClasses().values().stream()
-					.map((Class<?> c) -> c.getName())
+
+			this.supportedPipes = port.getSupportedPipeClasses().values().stream().map((Class<?> c) -> c.getName())
 					.collect(Collectors.toSet());
 		}
-		
+
 	}
-	
+
 	ModuleProfile(Module module) {
 		this.name = module.getName();
 		// TODO: The way to get a canonical class name should be standardised in Module
@@ -53,13 +65,11 @@ class ModuleProfile {
 		this.category = module.getCategory();
 		this.propertyDescriptions = module.getPropertyDescriptions();
 		this.propertyDefaultValues = module.getPropertyDefaultValues();
-		
-		this.inputPorts = module.getInputPorts().values().stream()
-				.map((Port p) -> new PortProfile(p))
+
+		this.inputPorts = module.getInputPorts().values().stream().map((Port p) -> new PortProfile(p))
 				.collect(Collectors.toList());
-		this.outputPorts = module.getOutputPorts().values().stream()
-				.map((Port p) -> new PortProfile(p))
+		this.outputPorts = module.getOutputPorts().values().stream().map((Port p) -> new PortProfile(p))
 				.collect(Collectors.toList());
 	}
-	
+
 }
